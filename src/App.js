83941f8904279222bf587c04e2f6cebac3b63410
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-11-02 18:13:42 +0530

    Implement SecondComponent with number increment using setInterval

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import { useState } from 'react'; // Import useState hook from React
+import { useState } from "react"; // Import useState hook from React
 
-import { MyComponent } from './components/MyComponent'; // Import the MyComponent component
+import { MyComponent } from "./components/MyComponent"; // Import the MyComponent component
+import { SecondComponent } from "./components/SecondComponent";
 
 function App() {
-    const [show, setShow] = useState(true); // Initialize state 'show' to true and create a function to update it
+  const [show, setShow] = useState(true); // Initialize state 'show' to true and create a function to update it
 
-    return (
-        <div>
-            {/* Button to remove the component by setting 'show' to false */}
-            <button onClick={() => setShow(false)}>Remove Component</button>
-            <br />
-            <br />
-            <br />
-            <br />
-            {/* Conditionally render MyComponent based on the value of 'show' */}
-            {show && <MyComponent />}
-        </div>
-    );
+  return (
+    <div>
+      {/* Button to remove the component by setting 'show' to false */}
+      <button onClick={() => setShow(false)}>Remove Component</button>
+      <br />
+      <br />
+      <br />
+      <br />
+      {/* Conditionally render MyComponent based on the value of 'show' */}
+      {show && (
+        <>
+        <MyComponent /> 
+        <SecondComponent />
+        </>
+      )}
+    </div>
+  );
 }
 
-export default App; 
+export default App;
--- src/components/SecondComponent.js
+++ src/components/SecondComponent.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import { useEffect, useState } from 'react'; 
 
-export default function SecondComponent() {
-  return (
-    <div>
-      
-    </div>
-  )
+export function SecondComponent() {
+    const [number, setNumber] = useState(0); 
+
+    useEffect(() => {
+
+        const interval = setInterval(() => { // Set up an interval to increment 'number' every second
+            setNumber((number) => number + 1); // Update 'number' by incrementing its current value
+            console.log("Number Incremented"); // Log message when number is incremented
+        }, 1000);
+
+        return () => clearInterval(interval); // Clear interval on component unmount to avoid memory leaks
+    }, []); // Empty dependency array ensures useEffect runs only once on mount
+
+    return (
+        <div>
+            <h1>Current Number : {number}</h1> {/* Display the current value of 'number' */}
+        </div>
+    );
 }
